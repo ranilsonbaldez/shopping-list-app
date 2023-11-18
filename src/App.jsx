dc48e13075bde1073fbc3bcf93431003fa7223b1
Ranilson <ranilson12@gmail.com>
2023-11-18 20:42:04 -0300

    creates header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <h1>Lista de Compras</h1>
+      <Header />
+      
+      <Footer />
     </>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export default function Header () {
+    return (
+        <header>
+            <h1>Lista de Compras</h1>
+        </header>
+    )
+}
