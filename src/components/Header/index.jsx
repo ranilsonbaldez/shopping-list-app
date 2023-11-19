5fd3e38020e390a0cc3e3f75951583c0e4e59007
Ranilson <ranilson12@gmail.com>
2023-11-18 21:21:11 -0300

    fix css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,9 @@ export default function Header () {
     return (
         <header>
             <h1>Lista de Compras</h1>
+            <div className="sum">
+                <p>R$ 700,00</p>
+            </div>
         </header>
     )
 }
